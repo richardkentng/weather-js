1fda8488f7f2b74b35158619c946fb7b41f6faeb
Richard Ng <richardkentng@gmail.com>
2021-05-02 21:57:01 -0700

    allows viewing of today's and tomorrow's weather. Display's message for invalid entry.

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const weather = require('weather-js')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+
 app.get('/', (req, res) => {
-    res.send('hi there')
+
+    const data = null
+    const zipCity = req.query.zipCity
+    if (zipCity) {
+        weather.find({search: zipCity, degreeType: 'F'}, (err, [data]) => {
+            console.log('-----');
+            console.log('data from random input===>', data);
+            res.render('index', {data})
+        })
+    } else {
+        res.render('index', {data})
+    }
+
+
+
 })
 
 
---
+++ sampleData.js
@@ -0,0 +1,79 @@
+{
+    location: { //data.location
+      name: 'Alhambra, CA',
+      zipcode: '91803',
+      lat: '34.077',
+      long: '-118.143',
+      timezone: '-7',
+      alert: '',
+      degreetype: 'F',
+      imagerelativeurl: 'http://blob.weather.microsoft.com/static/weather4/en-us/'
+    },
+    current: { //data.current
+      temperature: '59',
+      skycode: '31',
+      skytext: 'Clear',
+      date: '2021-05-02',
+      observationtime: '21:00:00',
+      observationpoint: '91803, CA',
+      feelslike: '60',
+      humidity: '73',
+      winddisplay: '3 mph Southwest',
+      day: 'Sunday',
+      shortday: 'Sun',
+      windspeed: '3 mph',
+      imageUrl: 'http://blob.weather.microsoft.com/static/weather4/en-us/law/31.gif'
+    },
+    forecast: [ //data.forecast
+      {
+        low: '54',
+        high: '76',
+        skycodeday: '27',
+        skytextday: 'Cloudy',
+        date: '2021-05-01',
+        day: 'Saturday',
+        shortday: 'Sat',
+        precip: ''
+      },
+      {
+        low: '54',
+        high: '71',
+        skycodeday: '30',
+        skytextday: 'Partly Sunny',
+        date: '2021-05-02',
+        day: 'Sunday',
+        shortday: 'Sun',
+        precip: '0'
+      },
+      {
+        low: '53',
+        high: '78',
+        skycodeday: '32',
+        skytextday: 'Sunny',
+        date: '2021-05-03',
+        day: 'Monday',
+        shortday: 'Mon',
+        precip: '0'
+      },
+      {
+        low: '58',
+        high: '83',
+        skycodeday: '30',
+        skytextday: 'Partly Sunny',
+        date: '2021-05-04',
+        day: 'Tuesday',
+        shortday: 'Tue',
+        precip: '0'
+      },
+      {
+        low: '59',
+        high: '85',
+        skycodeday: '32',
+        skytextday: 'Sunny',
+        date: '2021-05-05',
+        day: 'Wednesday',
+        shortday: 'Wed',
+        precip: '0'
+      }
+    ]
+  }
